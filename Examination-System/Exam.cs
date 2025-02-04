52349c5be8eac5cf0255d8164e21cd9d7ce47bfd
Mohamed Ramdan <mohamedramdan10.2001@gmail.com>
2025-02-04 22:09:00 +0200

    6-Design a Base class Exam [a. Time of exam b. Number of Questions , c. Show Exam Functionality]

---
+++ Examination-System/Exam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    internal abstract class Exam
+    {
+        #region Property
+        public int Time { get; set; }
+        public int NumberofQuestion { get; set; }
+        public Questions[]? Questions { get; set; }
+        #endregion
+
+        #region Constructor
+        public Exam(int time, int numberofQuestion, Questions[]? questions)
+        {
+            Time = time;
+            NumberofQuestion = numberofQuestion;
+            Questions = questions;
+        }
+        public Exam(int time, int numberofQuestion)
+        {
+            Time = time;
+            NumberofQuestion = numberofQuestion;
+        }
+        #endregion
+
+        #region Methods
+        public abstract void CreateExamListQuestions();
+        public abstract void ShowExam();
+        #endregion
+    }
+}
